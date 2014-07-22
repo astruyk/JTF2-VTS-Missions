662351bbdc2eed25dbc43f598c95eef25ea02816
anton <astruyk@gmail.com>
2014-07-21 20:20:50 -0400

    Add scripts to generate other island mission files
    Modified original script and added script to generate VTS maps for
    Chernarus, Takistan, Utes, Woodland_ACR and Zagarabad (the ones that the
    original server had).

--- scripts/Generate_PBO.bat
+++ scripts/Generate_PBO.bat
@@ -1,2 +1,15 @@
-..\lib\PBOManager_v_1_4_beta\PBOConsole.exe -pack ..\mission ..\bin\JTF2_Dynamic_VTS_XMED.Altis.pbo
-pause
+@echo off
+IF "%1"=="" GOTO no_args
+echo Generating PBO for mission '%1'
+SET MISSION_ISLAND=%1
+goto done
+:no_args
+echo No island specified, assuming Altis.
+SET MISSION_ISLAND=Altis
+:done
+@echo on
+set BASE_FILE_NAME=JTF2_Dynamic_VTS_XMED
+..\lib\PBOManager_v_1_4_beta\PBOConsole.exe -pack ..\mission ..\bin\%BASE_FILE_NAME%.%MISSION_ISLAND%.pbo
+if exist ..\bin\%BASE_FILE_NAME%.%MISSION_ISLAND%.pbo.bak del ..\bin\%BASE_FILE_NAME%.%MISSION_ISLAND%.pbo.bak
+@echo off
+echo Done generating PBO for '%MISSION_ISLAND%'
---
+++ scripts/Generate_PBO_AllMaps.bat
@@ -0,0 +1,6 @@
+call .\Generate_PBO.bat Altis
+call .\Generate_PBO.bat Chernarus
+call .\Generate_PBO.bat Takistan
+call .\Generate_PBO.bat Woodland_ACR
+call .\Generate_PBO.bat Utes
+call .\Generate_PBO.bat Zagarabad
