78492a8f2da1a2ba8eb2d74f0e8a81cdffef9ec8
anton <astruyk@gmail.com>
2014-07-25 08:17:48 -0400

    Setup PBO generation
    Remove the dynamic PBO files, remove ability to generate for all
    islands, update the generation script, and add the Deal_Or_No_Deal pbo
    instead.

--- scripts/Generate_PBO_AllMaps.bat
+++
@@ -1,6 +0,0 @@
-call .\Generate_PBO.bat Altis
-call .\Generate_PBO.bat Chernarus
-call .\Generate_PBO.bat Takistan
-call .\Generate_PBO.bat Woodland_ACR
-call .\Generate_PBO.bat Utes
-call .\Generate_PBO.bat Zagarabad
